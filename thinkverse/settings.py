65a7d7092e431224718220aaff6c890ad1f388af
Nikhil Agarwal <73541776+nikhil36agarwal@users.noreply.github.com>
2021-10-06 18:28:03 +0530

    Update settings.py

--- thinkverse/settings.py
+++ thinkverse/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-#import django_heroku
+import django_heroku
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
